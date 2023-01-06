12e8f709132719ea124eb2a56b94005dbbb419e7
Wesley Marques <Wesllety@gmail.com>
2023-01-06 20:36:33 -0300

    condiçoes

---
+++ curso JS/aula11/ex008.js
@@ -0,0 +1,9 @@
+let vel = 60.5
+
+if (vel <= 60){
+console.log(`A velocidade do carro é de ${vel}Km/h.`)
+
+}
+else{
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
---
+++ curso JS/aula11/ex009.js
@@ -0,0 +1,10 @@
+let pais = "EUA"
+
+console.log (`Se voce mora em ${pais} `)
+
+if (pais == "Brasil") {
+    console.log ("Brasileiro")
+}
+else {
+    console.log ("Você é gringo")
+}

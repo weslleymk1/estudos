1633ff8cbaffea02c4e9116d5608ed5526a0f96f
Wesley Marques <Wesllety@gmail.com>
2023-01-06 22:10:16 -0300

    Create ex013.js

---
+++ curso JS/aula12/ex013.js
@@ -0,0 +1,31 @@
+let agora = new Date()
+let diasem = agora.getDay()
+
+//diasem = 3
+
+switch(diasem){
+    case 0:
+        console.log("Domingo")
+        break
+    case 1:
+        console.log("Segunda")
+        break
+    case 2:
+        console.log("Terça")
+        break
+    case 3:
+        console.log("Quarta")
+        break
+    case 4:
+        console.log("Quinta")
+        break
+    case 5:
+        console.log("Sexta")
+        break
+    case 6:
+        console.log("Sábado")
+        break
+    default:
+        console.log("ERRO")
+        break
+}

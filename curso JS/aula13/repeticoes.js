2dd5d326aa59e76592d2ccc6aa1ed39ce35c2beb
Wesley Marques <Wesllety@gmail.com>
2023-01-21 14:07:53 -0300

    js

---
+++ curso JS/aula13/repeticoes.js
@@ -0,0 +1,29 @@
+// ------WHILE-------
+
+/*
+let c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+// -------DO WHILE ---------
+/*
+let c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+*/
+
+
+//-------FOR--------
+/*
+console.log(`VAI COMEÇAR...`)
+for (let c=1;c<=4;c++){
+    console.log(`passo ${c}`)
+}
+console.log(`FIM!`)
+*/
---
+++ curso JS/aula15/ambiente.js
@@ -0,0 +1,23 @@
+/*let num = [5,8,4]
+num[3] = 6
+num.push(7)
+num.sort()
+console.log(num)
+
+let num = [5,8,4,9,7,6]
+
+for (let pos = 0; pos<num.length; pos ++){
+    console.log(num[pos])
+}
+
+let num = [5,8,4,9,7,6]
+for(let pos in num){
+    console.log(num[pos])
+}*/
+
+let num = [5,8,4,9,7,6]
+console.log(num)
+
+let r = num.indexOf(9)
+
+console.log(r)
